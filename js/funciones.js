57ea37bf4a9dad7d450e60455006b3fe8d11c730
BooZhark <ju.guerrerod@duocuc.cl>
2024-05-12 17:15:19 -0400

    Update funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     $("#btnGenerar").click(function () {
         let rut = $("#txtRut").val();
         let nombre = $("#txtNombre").val();
@@ -7,7 +6,7 @@ $(document).ready(function () {
         let apmaterno = $("#txtApmaterno").val();
         let mail = $("#txtMail").val();
         let telefono = $("#txtTelefono").val();
-        let res = validarDatos(rut, nombre, appaterno, apmaterno, telefono)
+        let res = validarDatos(rut, nombre, appaterno, apmaterno, telefono, mail); 
         if (res) {
             $("#exampleModal").modal("show");
             $("#res").html("")
@@ -17,37 +16,34 @@ $(document).ready(function () {
             $("#res").append("Correo: " + mail + "<br>");
             $("#res").append("Telefono: " + telefono + "<br>");
             $("#res").append("</p>");
-
-            
             $("#formulario")[0].reset();
         }
     });
 
-    function validarDatos(rut, nombre, appaterno, apmaterno, celular, direccion) {
+    function validarDatos(rut, nombre, appaterno, apmaterno, celular, email) {
         if (String(rut).length < 9 || String(rut).length > 10) {
-            /* Validacion Rut */
             $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Rut debe tener largo entre 9 y 10 caracteres</div>");
         } else if (String(nombre).length < 3 || String(nombre).length > 20) {
-            /* Validacion Nombre */
             $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Nombre debe tener largo entre 3 y 20 caracteres</div>");
         } else if (String(appaterno).length <= 3 || String(appaterno).length >= 20) {
-            /* Validacion Apellido Paterno */
             $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Apellido Paterno debe tener largo entre 3 y 20 caracteres</div>");
         } else if (String(apmaterno).length <= 3 || String(apmaterno).length >= 20) {
-            /* Validacion Apellido Materno */
             $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Apellido Materno debe tener largo entre 3 y 20 caracteres</div>");
-        }   else if (String(celular).length < 9 || String(celular).length > 12) {
-            /* Validacion Celular */
+        } else if (String(celular).length < 9 || String(celular).length > 12) {
             $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Telefono debe tener entre 9 y 12 digitos</div>");
-        }  
-        else {
-            /* Confirmacion */
+        } else if (!validarCorreo(email)) { 
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Correo electrónico inválido</div>");
+        } else {
+            $("#check").html("");
             return true;
         }
-        
     }
+
     
-   
+    function validarCorreo(email) {
+        return email.includes('@');
+    }
+
     $("#btnEnviar").click(function () {
         $("#formulario")[0].reset();
     });
