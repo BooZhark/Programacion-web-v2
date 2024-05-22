489f12ebff4efad9fd591bd50a7a4cfebc25bf3d
TOeSCALNA <tomas.escalona03@gmail.com>
2024-05-22 16:34:08 -0400

    Update

--- Django/JyT/urls.py
+++ Django/JyT/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from JyT import views
+from . import views
 
-""" path('nombreURL',funcionVista,nombreDePagina) """
+
 urlpatterns = [
     path("", views.index, name="index"),
 ]
