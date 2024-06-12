e73f864b4974f3be14c42049a77b2e6b2a48e73a
CETECOM <ju.guerrerod@duocuc.cl>
2024-06-12 16:04:55 -0400

    Update views.py

--- Django/JyT/views.py
+++ Django/JyT/views.py
@@ -7,6 +7,130 @@ def index(request):
     }
     return render(request, "pages/index.html", context)
 
+def crud(request):
+    usuarios = Usuario.objects.all()
+    context = {
+        "usuarios": usuarios,
+    }
+    return render(request, "pages/crud.html", context)
+
+
+def user_add(request):
+    if request.method != "POST":
+        context = {
+        }
+        return render(request, "pages/user_add.html", context)
+    else:
+        rut = request.POST["rut"]
+        nombre = request.POST["nombre"]
+        appPaterno = request.POST["appPaterno"]
+        appMaterno = request.POST["appMaterno"]
+        fechaNac = request.POST["fecha"]
+        telefono = request.POST["telefono"]
+        correo = request.POST["correo"]
+        password = request.POST["password"]
+        direccion = request.POST["direccion"]
+        activo = True
+
+
+        obj = Usuario.objects.create(
+            rut=rut,
+            nombre=nombre,
+            apellido_paterno=appPaterno,
+            apellido_materno=appMaterno,
+            fecha_nacimiento=fechaNac,
+            telefono=telefono,
+            email=correo,
+            password=password,
+            direccion=direccion,
+            activo=activo,
+        )
+        obj.save()
+        context = {
+            "mensaje": "Registro Exitoso",
+        }
+        return render(request, "pages/user_add.html", context)
+
+
+def user_del(request, pk):
+    try:
+        usuario = Usuario.objects.get(rut=pk)
+        usuario.delete()
+
+        usuarios = Usuario.objects.all()
+        context = {
+            "mensaje": "Registro Eliminado",
+            "usuarios": usuarios,
+        }
+        return render(request, "pages/crud.html", context)
+    except:
+        usuarios = Usuario.objects.all()
+        context = {
+            "mensaje": "Error,Rut no encontrado...",
+            "usuarios": usuarios,
+        }
+        return render(request, "pages/crud.html", context)
+
+def user_findEdit(request,pk):
+    if pk!="":
+        """ 
+            objects.get() = Obtener datos con filtro
+            objects.all() = Obtener todos
+        """
+        usuario = Usuario.objects.get(rut=pk)
+
+        context={
+            "usuario":usuario,
+        }
+        return render(request,"pages/user_update.html",context)
+    else:
+        usuarios = Usuario.objects.all()
+        context={
+            "mensaje":"Error,Rut no encontrado",
+            "usuarios":usuarios
+        }
+        return render(request,"pages/crud.html",context)
+
+def user_update(request):
+    if request.method=="POST":
+        """ 
+            Capturo todos los datos del front
+            Identificamos
+            Asignamos nombre 
+        """
+        rut = request.POST["rut"]
+        nombre = request.POST["nombre"]
+        appPaterno = request.POST["appPaterno"]
+        appMaterno = request.POST["appMaterno"]
+        fechaNac = request.POST["fecha"]
+        telefono = request.POST["telefono"]
+        correo = request.POST["correo"]
+        password = request.POST["password"]
+        direccion = request.POST["direccion"]
+        activo = True
+
+        """ Genero la instancia """
+
+        obj = Usuario(
+            rut=rut,
+            nombre=nombre,
+            apellido_paterno=appPaterno,
+            apellido_materno=appMaterno,
+            fecha_nacimiento=fechaNac,
+            telefono=telefono,
+            email=correo,
+            password=password,
+            direccion=direccion,
+            activo=activo,
+        )
+        obj.save()
+
+        context = {
+            "mensaje": "Modificado con Exito",
+            "usuario":obj,
+        }
+        return render(request, "pages/user_update.html", context)
+
 def index(request):
     context = {}
     return render(request, "pages/contacto.html", context)
