1e259ccb6d91f1a32106895a4341a2f6a7bf3c66
BooZhark <ju.guerrerod@duocuc.cl>
2024-06-13 23:03:46 -0400

    1

--- Django/Django/urls.py
+++ Django/Django/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("JyT/", include("JyT.urls")),
+    path("", include("JyT.urls")),
 ]
--- Django/JyT/urls.py
+++ Django/JyT/urls.py
@@ -4,11 +4,15 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.contacto, name="contacto"),
-    path("", views.login, name="login"),
-    path("", views.monitores, name="monitores"),
-    path("", views.registro, name="registro"),
-    path("", views.sbnosotros, name="sbnosotros"),
-    path("", views.tarjvideo, name="tarjvideo"),
-    path("", views.user_add, name="user_add"),
+    path("contacto", views.contacto, name="contacto"),
+    path("login", views.login, name="login"),
+    path("monitores", views.monitores, name="monitores"),
+    path("registro", views.registro, name="registro"),
+    path("sbnosotros", views.sbnosotros, name="sbnosotros"),
+    path("tarvideo", views.tarjvideo, name="tarjvideo"),
+    path("crud", views.crud, name="crud"),
+    path("user_add", views.user_add, name="user_add"),
+    path("user_del/<str:pk>", views.user_del, name="user_del"),
+    path("user_findEdit/<str:pk>", views.user_findEdit, name="user_findEdit"),
+    path("user_update", views.user_update, name="user_update"),
 ]
